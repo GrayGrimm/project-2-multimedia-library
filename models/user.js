ef4d3cf173ef965b3b29f566d3c2c702b80fca4b
guyera <thegraygrimm@graymansurvivaltech.com>
2025-07-29 17:55:36 -0400

    created movieSchema

--- models/user.js
+++ models/user.js
@@ -1,6 +1,47 @@
 const mongoose = require('mongoose')
 const morgan = require('mongoose')
 
+const movieSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type: String,
+        enum: ['G', 'PG', 'PG-13', 'R', 'NC-17', 'NR'],
+        required: true,
+    },
+    genre: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['watched','read','played','listened', 'not yet'],
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    borrowable: {
+        type: Boolean,
+        required: true,
+    },
+    director: {
+        type: String,
+    },
+    realease_year: {
+        type: Number,
+    },
+    runtime_minutes: {
+        type: Number,
+    },
+    cast: {
+        type: String,
+    },
+});
+
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
